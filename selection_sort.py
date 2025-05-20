f581eb717dbeb789b89a86504da0687640b8fe59
juliadidra <juliadidra22@gmail.com>
2025-05-20 19:47:45 -0300

    feat: adding selection sort algorithm

---
+++ selection_sort.py
@@ -0,0 +1,23 @@
+
+#Example of selection sort algorithm
+
+def searchMinorElement(arr):
+    minor = arr[0] # get the first element
+    minorIndex = 0 # initialize with the minor index, which is 0
+
+    for i in range(1, len(arr)): # iterate trhough the array (starting from 1)
+        if arr[i] < minor: # compares if the current element is less than the minor (in the first iteration, the comparison is with the first element of the array)
+
+            minor = arr[i] # if the condition is true, the current element is the new minor
+            minorIndex = i #update the minor index with the current index
+    return minorIndex
+
+def selectionSort(arr):
+    newArr = []
+
+    for i in range(len(arr)): # iterate through the array
+        minor = searchMinorElement(arr) #find the minor element 
+        newArr.append(arr.pop(minor)) # remove the minor element of the array and append it to the new array, the values will be added in ascending order
+    return newArr
+
+print(selectionSort([5, 3, 6, 2, 10]))
